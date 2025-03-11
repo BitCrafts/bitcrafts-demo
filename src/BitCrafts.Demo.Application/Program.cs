6db85c1dfcdee59e8043e8ca026379be0caeca2f
Younes Benmoussa <180306835+BitCrafts@users.noreply.github.com>
2025-03-11 17:03:46 +0000

    project setup

---
+++ src/BitCrafts.Demo.Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using BitCrafts.Infrastructure.Abstraction.Application;
+using BitCrafts.Infrastructure.Application.Avalonia.Extensions;
+using BitCrafts.Infrastructure.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services
+    .AddBitCraftsInfrastructure()
+    .AddBitCraftsAvaloniaApplication();
+
+var provider = services.BuildServiceProvider();
+var factory = provider.GetService<IApplicationFactory>();
+if (factory == null)
+    throw new NullReferenceException("factory is null");
+
+var app = factory.CreateApplication();
+app.ServiceProvider = provider;
+await app.StartAsync();
+app.Dispose();
